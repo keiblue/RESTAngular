b8a15310a1a28e5042a87270973375827d4245a3
kei <f.malermo@gmail.com>
2019-06-20 07:43:06 -0400

    primer commit}

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
+import { ListCustomerComponent } from './components/list-customer/list-customer.component';
+
+// Importar rutas
+import { ROUTES } from './app.routes';
+import { RouterModule } from '@angular/router';
+
+// Importar ReactiveFormsModule para los formularios
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddCustomerComponent,
+    EditCustomerComponent,
+    ListCustomerComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, { useHash: true } ),
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
